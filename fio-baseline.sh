023ca238bab70110aff6f1f6e78b09a3d62de6a9
Andy Wang <andy.wang@scaleflux>
2020-10-12 19:17:47 +0800

    add missing comp string

--- fio-baseline.sh
+++ fio-baseline.sh
@@ -24,7 +24,10 @@ pid_list=""
 for disk in ${disks[@]};
 do
     iostat -dxmct 1 ${disk} > ${timestamp}/${disk}.iostat &
-    fio --filename=/dev/${disk} --output=${timestamp}/${disk}_fio.log ./baseline.fio &
+    fio "${comp_opt_str}" \
+        --filename=/dev/${disk} \
+        --output=${timestamp}/${disk}.fio \
+        ./baseline.fio &
     pid_list="${pid_list} $!"
 done
 
